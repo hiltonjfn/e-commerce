93fe29a96959630251617f48d41fd1319e7cf8f1
hiltonjfn <88454528+hiltonjfn@users.noreply.github.com>
2022-08-18 02:46:17 -0300

    Add files via upload

---
+++ js/pesquisar.js
@@ -0,0 +1,11 @@
+function google(x){
+        document.getElementById('main_principal').innerHTML = '';
+
+    
+        for(z in lista){
+            if(lista[z].Nome.slice(0,x.length).toLowerCase() == x.toLowerCase()){
+                gerar_arquivo_base(lista[z].Nome,lista[z].img,lista[z].descricao.slice(0,80)+"...",'harry',lista[z].preco)
+            }
+        }
+
+}
